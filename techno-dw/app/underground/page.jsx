64c828ea4b8bd0e931758e0159b47ce8e30dff22
Rafael David <rafaelcapitaodavid@gmail.com>
2023-12-12 16:31:40 +0000

    deploy dev test process.env

--- techno-dw/app/underground/page.jsx
+++ techno-dw/app/underground/page.jsx
@@ -1,13 +1,13 @@
 import { getGlobalData } from "@/lib/cosmic";
 import WrappedText from "../components/WrappedText"
 
-export async function generateMetadata() {
+/* export async function generateMetadata() {
   const siteData = await getGlobalData();
   return {
     title: siteData.metadata.site_title,
     description: siteData.metadata.site_tag,
   };
-}
+} */
 
 export default async function Underground() {
   return (

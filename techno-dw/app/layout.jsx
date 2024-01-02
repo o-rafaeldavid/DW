126b55073c030b85c3e00451dd476b27a5d7a48f
Rafael David <rafaelcapitaodavid@gmail.com>
2024-01-02 14:46:30 +0000

    add fonts

--- techno-dw/app/layout.jsx
+++ techno-dw/app/layout.jsx
@@ -2,7 +2,12 @@ import Body from './body'
 import { getGlobalData } from '@/lib/cosmic'
 
 import main from "../styles/modules/main.module.scss"
+import { Inter } from 'next/font/google'
+import { Lexend_Peta } from 'next/font/google'
 import "../styles/general.scss"
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter'})
+const lexend_peta = Lexend_Peta({ subsets: ['latin'], variable: '--font-lexend-peta'})
 
 export async function generateMetadata() {
   const metaDataPrincipal = await getGlobalData()
@@ -17,7 +22,7 @@ export default async function RootLayout({ children }) {
   return (
     <>
       <html lang="en" className={main.fundo}>
-        <Body className={main.body}>
+        <Body className={`${main.body} ${inter.variable} ${lexend_peta.variable}`}>
           {children}
         </Body>
       </html>

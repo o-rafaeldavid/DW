91849a7d8a044a4ef6bfa13eec95cc20b7ab29a8
Rafael David <rafaelcapitaodavid@gmail.com>
2024-01-02 14:21:10 +0000

    fonts

--- techno-dw/app/layout.jsx
+++ techno-dw/app/layout.jsx
@@ -3,8 +3,6 @@ import { getGlobalData } from '@/lib/cosmic'
 
 import main from "../styles/modules/main.module.scss"
 import "../styles/general.scss"
-import Head from 'next/head'
-
 
 export async function generateMetadata() {
   const metaDataPrincipal = await getGlobalData()
@@ -19,12 +17,6 @@ export default async function RootLayout({ children }) {
   return (
     <>
       <html lang="en" className={main.fundo}>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-          <link href="https://fonts.googleapis.com/css2?family=Lexend+Peta:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-        </Head>
         <Body className={main.body}>
           {children}
         </Body>
